8064b7ce31cfceab2a65c467d279e08e07401400
David <mail@davidfoale.info>
2016-07-11 21:40:48 +0000

    first commit

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+/*Configuration Settings*/
+
+define('DB_HOST', 'localhost'); /*Database Server*/
+define('DB_NAME', 'formcollector'); /*Database Name*/
+define('DB_USER', 'root'); /*Database Username*/
+define('DB_PWD',  ''); /*Database Password*/
+
+?>
