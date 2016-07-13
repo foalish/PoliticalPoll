9f2adba11c9452a41e0a7fb1e8d9d82fbacf4639
David Foale <mail@davidfoale.info>
2016-07-13 15:34:22 +0100

    Add files via upload

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 /*Configuration Settings*/
 
 define('DB_HOST', 'localhost'); /*Database Server*/
-define('DB_NAME', 'formcollector'); /*Database Name*/
-define('DB_USER', 'root'); /*Database Username*/
-define('DB_PWD',  ''); /*Database Password*/
+define('DB_NAME', 'cl47-poll'); /*Database Name*/
+define('DB_USER', 'cl47-poll'); /*Database Username*/
+define('DB_PWD',  'chocolate100'); /*Database Password*/
 
 ?>
